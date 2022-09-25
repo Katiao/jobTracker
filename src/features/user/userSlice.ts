90d410912c7da434004fe1b2ed15d499afe06a7c
Katiao <katiao_@hotmail.co.uk>
2022-09-25 13:32:55 +0200

    Implement correct type for extraReducers, fix type errors

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import { MODEL__user, MODEL__Member, MODEL__NonMember } from "../../types";
@@ -22,10 +21,9 @@ export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user: MODEL__NonMember, thunkAPI) => {
     try {
-      //add resp type
-      const resp = await customFetch.post("/auth/register", user);
+      const resp = await customFetch.post<InitiaState>("/auth/register", user);
       return resp.data;
-    } catch (error) {
+    } catch (error: any) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
@@ -36,10 +34,9 @@ export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user: MODEL__Member, thunkAPI) => {
     try {
-      //add resp type
-      const resp = await customFetch.post("/auth/login", user);
+      const resp = await customFetch.post<InitiaState>("/auth/login", user);
       return resp.data;
-    } catch (error) {
+    } catch (error: any) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
@@ -48,35 +45,41 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
-  extraReducers: {
-    [registerUser.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [registerUser.fulfilled]: (state, { payload }) => {
-      const { user } = payload;
-      state.isLoading = false;
-      state.user = user;
-      addUserToLocalStorage(user);
-      toast.success(`Hello There ${user.name}`);
-    },
-    [registerUser.rejected]: (state, { payload }) => {
-      state.isLoading = false;
-      toast.error(payload);
-    },
-    [loginUser.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [loginUser.fulfilled]: (state, { payload }) => {
-      const { user } = payload;
-      state.isLoading = false;
-      state.user = user;
-      addUserToLocalStorage(user);
-      toast.success(`Welcome back ${user.name}`);
-    },
-    [loginUser.rejected]: (state, { payload }) => {
-      state.isLoading = false;
-      toast.error(payload);
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    return (
+      builder.addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      }),
+      builder.addCase(
+        registerUser.fulfilled,
+        (state, { payload: { user } }) => {
+          state.isLoading = false;
+          state.user = user;
+          addUserToLocalStorage(user);
+          toast.success(`Hello There ${user?.name}`);
+        }
+      ),
+      builder.addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        const { payload } = action;
+        toast.error(payload as string);
+      }),
+      builder.addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      }),
+      builder.addCase(loginUser.fulfilled, (state, { payload: { user } }) => {
+        state.isLoading = false;
+        state.user = user;
+        addUserToLocalStorage(user);
+        toast.success(`Welcome back ${user?.name}`);
+      }),
+      builder.addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        const { payload } = action;
+        toast.error(payload as string);
+      })
+    );
   },
 });
 
--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -3,7 +3,7 @@ import { MODEL__user } from "../types";
 // When user navigates away from the page, user details are lost
 //Therefore save to local storage
 
-export const addUserToLocalStorage = (user: MODEL__user) => {
+export const addUserToLocalStorage = (user: MODEL__user | null) => {
   localStorage.setItem("user", JSON.stringify(user));
 };
 
@@ -14,6 +14,5 @@ export const removeUserFromLocalStorage = () => {
 //check if there is a user in local storage first, otherwise return null
 export const getUserFromLocalStorage = () => {
   const result = localStorage.getItem("user");
-  console.log(result, "result from local storage");
   return result ? JSON.parse(result) : null;
 };

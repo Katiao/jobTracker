09face307e94880a4725e5d932f4dfdd1c1ba587
Katiao <katiao_@hotmail.co.uk>
2022-11-23 17:17:34 +0100

    Set up server and connect to databse, create error handler and not found middleware update package.json and install dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
-console.log("server running...");
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.js";
+
+//middleware
+import notFoundMiddleWare from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+//only spin up server if connection to database successful
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

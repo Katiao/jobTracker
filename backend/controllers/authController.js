fd392b5b91ce12cb997598fa00efc4a5d84dbc7a
Katiao <katiao_@hotmail.co.uk>
2022-11-23 20:31:10 +0100

    controllers and routes set up and structure

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+  res.send("register user");
+};
+
+export const login = async (req, res) => {
+  res.send("log in user");
+};
+
+export const updateUser = async (req, res) => {
+  res.send("update user");
+};
--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,33 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// db and authenticate user
 import connectDB from "./db/connect.js";
 
-//middleware
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
+// middleware
 import notFoundMiddleWare from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+// to make json data available to us in the controllers
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-//only spin up server if connection to database successful
+// only spin up server if connection to database successful
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);

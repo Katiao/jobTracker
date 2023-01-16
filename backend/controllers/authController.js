a6c7ed26da9049f5d71f3c9b35e98fcc211d08b2
Katiao <katiao_@hotmail.co.uk>
2023-01-16 20:31:36 +0100

    hash password and JWT token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -20,7 +20,16 @@ export const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+  });
 };
 
 export const login = async (req, res) => {

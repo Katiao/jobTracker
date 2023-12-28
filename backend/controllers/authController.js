bc1136de08dd9e4340f2f60e109f1802084e0967
Katiao <katiao_@hotmail.co.uk>
2023-12-28 12:00:13 +0100

    Migrate to Vite and add backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,62 +1,43 @@
 import { StatusCodes } from "http-status-codes";
-import User from "../models/User.js";
-import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
+import User from "../models/UserModel.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
-// Note: no need to use try catch block to pass on error to error middleware
-// as using "express-async-errors" package
 export const register = async (req, res) => {
-  // Good to check for some errors (empty values/ duplicate email) in controller before it hits error middleware
-  const { name, email, password } = req.body;
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
 
-  if (!name || !email || !password) {
-    // express async errors package - no need to use next
-    throw new BadRequestError("Please provide all values");
-  }
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
 
-  const userAlreadyExists = await User.findOne({ email });
-
-  if (userAlreadyExists) {
-    throw new BadRequestError("Email already in use");
-  }
-
-  const user = await User.create({ name, email, password });
-  const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({
-    user: {
-      email: user.email,
-      lastName: user.lastName,
-      location: user.location,
-      name: user.name,
-    },
-    token,
-  });
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
-
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    throw new BadRequestError("Please provide all values");
-  }
+  const user = await User.findOne({ email: req.body.email });
 
-  // need to select password due to "select: false" in User password
-  const user = await User.findOne({ email }).select("+password");
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
 
-  if (!user) {
-    throw new UnauthenticatedError("Invalid Credentials");
-  }
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
 
-  const isPasswordCorrect = await user.comparePassword(password);
-  if (!isPasswordCorrect) {
-    throw new UnauthenticatedError("Invalid Credentials");
-  }
+  const token = createJWT({ userId: user._id, role: user.role });
 
-  const token = user.createJWT();
-  // Remove password from response
-  user.password = undefined;
-  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
 
-export const updateUser = async (req, res) => {
-  console.log(req.user);
-  res.send("update user");
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,74 @@
+import "express-async-errors";
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 const app = express();
-
-import dotenv from "dotenv";
-dotenv.config();
-import "express-async-errors";
 import morgan from "morgan";
-
-// db and authenticate user
-import connectDB from "./db/connect.js";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 
 // routers
-import authRouter from "./routes/authRoutes.js";
-import jobsRouter from "./routes/jobsRoutes.js";
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
+// public
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // middleware
-import notFoundMiddleWare from "./middleware/not-found.js";
-import errorHandlerMiddleware from "./middleware/error-handler.js";
-import authenticateUser from "./middleware/auth.js";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(morgan("dev"));
-}
-
-// to make json data available to us in the controllers
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcome!");
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-//restricted: requires token
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
 
-app.use(notFoundMiddleWare);
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5100;
 
-// only spin up server if connection to database successful
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
